44c0951b9eb2438a7ade9b4f982f2dfe3e750857
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-06-13 20:02:35 +0530

    First Commit

---
+++ src/dummy-products.js
@@ -0,0 +1,58 @@
+import mochaOvercoat from './assets/mocha-overcoat.jpg';
+import dreamGown from './assets/dream-gown.jpg';
+import rainJacket from './assets/rain-jacket.jpg';
+import merlotSuit from './assets/merlot-suit.jpg';
+import moonlightDress from './assets/moonlight-dress.jpg';
+import denimPioneer from './assets/denim-pioneer.jpg';
+
+export const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    image: mochaOvercoat,
+    title: 'Majestic Vintage Mocha Overcoat',
+    price: 129.99,
+    description:
+      'Channel timeles blend of vintage charm and modern elegance.',
+  },
+  {
+    id: 'p2',
+    image: dreamGown,
+    title: 'Enchanting Blush Dream Gown',
+    price: 189.99,
+    description:
+      ' that whispers enchantment. This is the perfect piece for those seeking to create unforgettable moments.',
+  },
+
+  {
+    id: 'p3',
+    image: rainJacket,
+    title: 'Vintage Sunshine Rain Jacket',
+    price: 49.99,
+    description:
+      ' is never a reason to compromise on your fashion quotient.',
+  },
+  {
+    id: 'p4',
+    image: merlotSuit,
+    title: 'Classic Merlot Business Suit',
+    price: 249.99,
+    description:
+    ' and understated power, it is ideal for the modern executive who values tradition and elegance.',
+    },
+    {
+    id: 'p5',
+    image: moonlightDress,
+    title: 'Ethereal Moonlight Evening Dress',
+    price: 159.99,
+    description:
+    'Sweep  dress is a nod to those who appreciate subtle glamour and a standout silhouette.',
+    },
+    {
+    id: 'p6',
+    image: denimPioneer,
+    title: 'Pioneer Rugged Denim Jacket',
+    price: 79.99,
+    description:
+    'Our Pioneer Rugged Denim Ja, this jacket is a wardrobe essential for the urban explorer.'
+    }
+];
